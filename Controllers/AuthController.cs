d4dca249d5513da30c3b9fc8c3170d2174af041b
dinesh lohani <dineshlohani333@gmail.com>
2022-06-19 08:52:02 +0545

    initial commit

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace LibraryManagementSystem.Controllers
+{
+    public class AuthController : Controller
+    {
+        public ViewResult Signup()
+        {
+            return View();
+        }
+
+        public ViewResult Login()
+        {
+            return View();
+        }
+
+        public ViewResult Dashboard()
+        {
+            return View();
+        }
+
+        public ViewResult Addbook()
+        {
+            return View();
+        }
+
+        public ViewResult Book()
+        {
+            return View();
+        }
+
+
+        public ViewResult Vehicle()
+        {
+            return View();
+        }
+
+        public ViewResult addService()
+        {
+            return View();
+        }
+        public ViewResult Remove()
+        {
+            return View();
+        }
+        public ViewResult Removemember()
+        {
+            return View();
+        }
+        public ViewResult SearchBook()
+        {
+            return View();
+        }
+        public ViewResult Borrow()
+        {
+            return View();
+        }
+        public ViewResult Return()
+        {
+            return View();
+        }
+    }
+}
