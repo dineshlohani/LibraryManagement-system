3ca5a4c35ab96a19d163f1e96e3c6b7ee0cd76ec
dinesh lohani <dineshlohani333@gmail.com>
2022-06-19 19:02:44 +0545

    second commit

---
+++ Models/Addbook.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagementSystem.Models
+{
+    public class Addbook
+    {
+        [key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+       [EmailAddress]
+       public string Email { get; set; }
+
+
+    }
+
+    internal class keyAttribute : Attribute
+    {
+    }
+}
